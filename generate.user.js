2eb6b2a62b1686f82234f15e8470768bd3bddb43
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-06-23 19:28:59 +0800

    文件读取测试

--- generate.user.js
+++ generate.user.js
@@ -7,11 +7,11 @@
 // @match        https://steamcommunity.com/id/*/games/*
 // @icon         https://steamcommunity.com/favicon.ico
 // @grant        none
-// @require      https://lib.baomitu.com/pako/2.0.4/pako.min.js
+// @require      https://lib.baomitu.com/pako/2.0.4/pako.min.js#sha512-EAKzSKex+PXC0U9OG13r1059ysjrjkJEpZoONCnZa0mBROY28iBOOxZSErUVw1LzLr2+U5PhR7zPCPKidUVJqg==
 // @run-at       document-idle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     function getVideoCardInfo() {
         const gl = document.createElement('canvas').getContext('webgl');
@@ -24,30 +24,31 @@
             renderer: gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
         } : false;
     }
-    if(document.querySelector("#account_pulldown").innerText.trim()==document.querySelector("#responsive_page_template_content > div.pagecontent.no_header > div.profile_small_header_bg > div > div > span.profile_small_header_name > a").innerText.trim()){
-        let dumpBtn=document.createElement("a");
-        dumpBtn.innerText="保存游戏清单";
-        document.querySelector("#gameslist_sort_options").insertAdjacentElement("afterbegin",dumpBtn);
-        dumpBtn.addEventListener("click",async ()=>{
-            let toSave={};
-            toSave.username=document.querySelector("#account_pulldown").innerText.trim();
-            toSave.userid=g_steamID;
-            toSave.games=rgGames;
+    if (document.querySelector("#account_pulldown").innerText.trim() == document.querySelector("#responsive_page_template_content > div.pagecontent.no_header > div.profile_small_header_bg > div > div > span.profile_small_header_name > a").innerText.trim()) {
+        let dumpBtn = document.createElement("a");
+        dumpBtn.innerText = "保存游戏清单";
+        document.querySelector("#gameslist_sort_options").insertAdjacentElement("afterbegin", dumpBtn);
+        dumpBtn.addEventListener("click", async () => {
+            let toSave = {};
+            toSave.username = document.querySelector("#account_pulldown").innerText.trim();
+            toSave.userid = g_steamID;
+            toSave.games = rgGames;
             toSave.GPUinfo = getVideoCardInfo();
-            let userAgent=await navigator.userAgentData.getHighEntropyValues(['architecture', 'bitness', 'mobile', 'platform', 'platformVersion', 'uaFullVersion', 'wow64'])
+            toSave.time = Date.now();
+            let userAgent = await navigator.userAgentData.getHighEntropyValues(['architecture', 'bitness', 'mobile', 'platform', 'platformVersion', 'uaFullVersion', 'wow64'])
             delete userAgent.brands;
-            toSave.browser=userAgent;
-            let toSaveJson=JSON.stringify(toSave);
+            toSave.browser = userAgent;
+            let toSaveJson = JSON.stringify(toSave);
             console.log(toSave);
             let eleLink = document.createElement('a');
-            eleLink.download = g_steamID+".svk";
+            eleLink.download = g_steamID + ".svk";
             eleLink.style.display = 'none';
             eleLink.href = URL.createObjectURL(new Blob([pako.gzip(toSaveJson)]));
             document.body.appendChild(eleLink);
             eleLink.click();
             document.body.removeChild(eleLink);
         })
-    }else{
+    } else {
         console.log("Not owner!")
     }
 })();
